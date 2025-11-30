e5c3a45c91af14b5ac50977c279aa7956bdb2041
Claudio Vasquez <clauvzq150@gmail.com>
2025-11-30 00:39:23 -0300

    Initial commit.

---
+++ src/main/java/org/khenas/customScoreboard/CustomScoreboard.java
@@ -0,0 +1,17 @@
+package org.khenas.customScoreboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomScoreboard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
